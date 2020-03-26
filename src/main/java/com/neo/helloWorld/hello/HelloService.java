57a46917703fb6e9d77c0995625571c36aa4695d
liukai <578123466@qq.com>
2020-03-27 00:43:14 +0800

    SLOG-第一天

---
+++ src/main/java/com/neo/helloWorld/hello/HelloService.java
@@ -0,0 +1,14 @@
+package com.neo.helloWorld.hello;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Lenovo on 2018/9/4.
+ */
+@Service
+public class HelloService {
+
+    public void sayHello () {
+        System.out.println("Hello World");
+    }
+}
